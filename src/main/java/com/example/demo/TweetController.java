250b152ab4ea474fd56c57f33fd7eb2fced982ce
Jaired Jawed <jairedjawed@icloud.com>
2022-06-02 15:29:21 -0700

    Created Search Page

--- src/main/java/com/example/demo/TweetController.java
+++ src/main/java/com/example/demo/TweetController.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+
 
 
 @RestController
@@ -11,7 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TweetController {
 
     @RequestMapping("/tweets")
-    public String index() {
+    public String index(@RequestParam(required = false, defaultValue = "") String query) {
+        System.out.println("Query: " + query);
+
+        Analyzer analyzer = new StandardAnalyzer();
+
+        if (query.isEmpty()) {
+            return "No query provided.";
+        }
+
         return "Hello Spring Boot!";
     }
 

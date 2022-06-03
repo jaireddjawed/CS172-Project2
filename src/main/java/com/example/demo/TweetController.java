68705f9c7ef9fa1b88fdd9eea8e1d765c1bb0a8c
Jaired Jawed <jairedjawed@icloud.com>
2022-06-02 18:09:32 -0700

    Print every tweet from the tweets file

--- src/main/java/com/example/demo/TweetController.java
+++ src/main/java/com/example/demo/TweetController.java
@@ -7,45 +7,48 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.Query;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin("*")
 public class TweetController {
-    static ArrayList<Tweet> tweets;
-    static int numTweetFiles = 0;
+    // todo: change this to the number of tweet files you have in the tweets folder
+    static int numTweetFiles = 5;
 
     @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() throws IOException {
-        tweets = new ArrayList<Tweet>();
+    public void loadTweets() throws FileNotFoundException, IOException, org.json.simple.parser.ParseException {
+        JSONParser parser = new JSONParser();
 
+        // iterate through all of the tweet files
+        for (int i = 1; i <= numTweetFiles; i++) {
+            Object tweetFile = parser.parse(
+                new FileReader("./tweets/tweets_"+i+"_with_titles.json")
+            );
 
-        try {
-            File myObj = new File("tweets.txt");
-      Scanner reader = new Scanner(myObj);
-            while (reader.hasNextLine()) {
-                String data = reader.nextLine();
-                System.out.println(data);
-              }
-            reader.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            JSONArray tweets = (JSONArray) tweetFile;
+            var tweetIterator = tweets.iterator();
+
+            while(tweetIterator.hasNext()) {
+                JSONObject tweet = (JSONObject) tweetIterator.next();
+                System.out.println(tweet);
+            }
         }
     }
 
